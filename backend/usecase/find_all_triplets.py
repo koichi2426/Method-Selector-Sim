d83416a0167a02b82bd0d2b5f76b8c1e4dd6e418
koichi <111332153+koichi2426@users.noreply.github.com>
2025-07-19 12:38:31 +0900

    find_all_triplets.py実装

---
+++ backend/usecase/find_all_triplets.py
@@ -0,0 +1,63 @@
+import abc
+import uuid
+from dataclasses import dataclass
+from typing import Protocol, List
+
+
+from domain import (
+    Triplet,
+    TripletRepository,
+)
+
+
+class FindAllTripletsUseCase(Protocol):
+    def execute(self) -> tuple[List["FindAllTripletsOutput"], Exception | None]:
+        ...
+
+
+@dataclass
+class FindAllTripletsOutput:
+    ID: uuid.UUID
+    TrainingReadyScenario_ID: uuid.UUID
+    anchor: str
+    positive: str
+    negative: str
+
+
+class FindAllTripletsPresenter(abc.ABC):
+    @abc.abstractmethod
+    def output(self, triplets: List[Triplet]) -> List["FindAllTripletsOutput"]:
+        pass
+
+
+class FindAllTripletsInteractor:
+    def __init__(
+        self,
+        presenter: "FindAllTripletsPresenter",
+        repo: TripletRepository,
+        timeout_sec: int = 10,
+    ):
+        self.presenter = presenter
+        self.repo = repo
+        self.timeout_sec = timeout_sec
+
+    def execute(self) -> tuple[List["FindAllTripletsOutput"], Exception | None]:
+        try:
+            triplets = self.repo.find_all()
+            output = self.presenter.output(triplets)
+            return output, None
+
+        except Exception as e:
+            return [], e
+
+
+def new_find_all_triplets_interactor(
+    presenter: "FindAllTripletsPresenter",
+    repo: TripletRepository,
+    timeout_sec: int,
+) -> "FindAllTripletsUseCase":
+    return FindAllTripletsInteractor(
+        presenter=presenter,
+        repo=repo,
+        timeout_sec=timeout_sec,
+    )
