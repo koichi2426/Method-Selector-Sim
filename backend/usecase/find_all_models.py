f6d3110775a30c948f75be01298c2a5bb1e5f8dc
koichi <111332153+koichi2426@users.noreply.github.com>
2025-07-19 13:13:10 +0900

    find_all_models.py実装

---
+++ backend/usecase/find_all_models.py
@@ -0,0 +1,64 @@
+import abc
+import uuid
+from dataclasses import dataclass
+from typing import Protocol, List
+from datetime import datetime
+
+from domain import (
+    TrainedModel,
+    TrainedModelRepository,
+)
+
+
+class FindAllModelsUseCase(Protocol):
+    def execute(self) -> tuple[List["FindAllModelsOutput"], Exception | None]:
+        ...
+
+
+@dataclass
+class FindAllModelsOutput:
+    ID: uuid.UUID
+    name: str
+    Dataset_ID: uuid.UUID
+    description: str
+    file_path: str
+    created_at: str
+
+
+class FindAllModelsPresenter(abc.ABC):
+    @abc.abstractmethod
+    def output(self, models: List[TrainedModel]) -> List["FindAllModelsOutput"]:
+        pass
+
+
+class FindAllModelsInteractor:
+    def __init__(
+        self,
+        presenter: "FindAllModelsPresenter",
+        repo: TrainedModelRepository,
+        timeout_sec: int = 10,
+    ):
+        self.presenter = presenter
+        self.repo = repo
+        self.timeout_sec = timeout_sec
+
+    def execute(self) -> tuple[List["FindAllModelsOutput"], Exception | None]:
+        try:
+            models = self.repo.find_all()
+            output = self.presenter.output(models)
+            return output, None
+
+        except Exception as e:
+            return [], e
+
+
+def new_find_all_models_interactor(
+    presenter: "FindAllModelsPresenter",
+    repo: TrainedModelRepository,
+    timeout_sec: int,
+) -> "FindAllModelsUseCase":
+    return FindAllModelsInteractor(
+        presenter=presenter,
+        repo=repo,
+        timeout_sec=timeout_sec,
+    )
